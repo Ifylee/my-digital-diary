6274c8957224fb8b5885af2d828890ca3ed51a95
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-06-01 08:43:01 -0500

    more comments

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,7 @@
 const mainElement = document.querySelector("main");
 
+// this function returns a newly created elements to the DOM.
+// this function takes three parameters: the type of element created, the text content value and the parent of the newly created element.
 const renderElement = function(type, text, parent) {
     const element = document.createElement("h2");
     element.textContent = "No blogs available yet";
@@ -8,30 +10,42 @@ const renderElement = function(type, text, parent) {
 
     return element;
 }
-
-const handleEmpty = function() {
+// this function provides flexibility in generating and adding elements to a webpage.
+const handleEmpty = function(type, text, parent) {
     const element = document.createElement(type);
     element.textContent = text;
     parent.appendChild(element);
 }
 
+// this function gets the stored blog data
 const renderBlogList = function() {
     const blogs = readBlogDataFromLocalStorage();
 
+// checks for empty blogs, if trthy then the handleEmpty function is called.
     if(blogs.length === 0) {
         handleEmpty();
 
         return;
     }
 
+// this loops through an array of blogs and creates HTML elements to represent each blog entry.
     for(let i = 0; i < blogs.length; i++) {
+
+        // article element created to contain the blog content within it
         const article = document.createElement("article");
 
+       // HTML elements being created here. 
+       // <h2> for the blog title, 
         const h2 = document.createElement("h2");
+
+       //<blockquote> for the content. 
         const blockquote = document.createElement("blockquote");
+
+        // <p> for the author.
         const p = document.createElement("p");
         h2.textContent = blogs[i].title;
-
+        
+        //appending elements
         article.appendChild(h2);
 
         blockquote.textContent = blogs[i].content;
@@ -39,8 +53,11 @@ const renderBlogList = function() {
 
         p.textContent = `By: ${blogs[i].username}`;
         article.appendChild(p);
+
+        // css class name of card added to the article element so it looks like a card.
         article.classList.add('card');
 
+        // parent element
         mainElement.appendChild(article);    
     }
 }
